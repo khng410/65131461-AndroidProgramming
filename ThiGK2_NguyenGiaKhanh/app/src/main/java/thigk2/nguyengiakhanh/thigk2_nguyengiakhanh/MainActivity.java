1c6aed4be5c7b59f197e3eef9d45825defe54258
KhanhNguyen <ngkhanh4102005@gmail,com>
2026-04-20 07:42:25 +0700

    Commit Lan 1

--- ThiGK2_NguyenGiaKhanh/app/src/main/java/thigk2/nguyengiakhanh/thigk2_nguyengiakhanh/MainActivity.java
+++ ThiGK2_NguyenGiaKhanh/app/src/main/java/thigk2/nguyengiakhanh/thigk2_nguyengiakhanh/MainActivity.java
@@ -8,13 +8,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
+    private Button mainBtn1, mainBtn2, mainBtn3, mainBtn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        mainBtn1 = findViewById(R.id.main_btn1);
+        mainBtn2 = findViewById(R.id.main_btn2);
+        mainBtn3 = findViewById(R.id.main_btn3);
+        mainBtn4 = findViewById(R.id.main_btn4);
+
+        mainBtn1.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity2.class);
+            intent.putExtra("signal", "bmi");
+            startActivity(intent);
+        });
+
+        mainBtn2.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity2.class);
+            intent.putExtra("signal", "food");
+            startActivity(intent);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
