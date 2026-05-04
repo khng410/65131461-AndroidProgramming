0400811a18056eb582cdd0637fd51a3f17e4e9bc
KhanhNguyen <ngkhanh4102005@gmail,com>
2026-05-04 21:20:32 +0700

    Tao app VCL - Phan 2

--- ViecCanLam/app/src/main/java/com/edu/nguyengiakhanh/vieccanlam/MainActivity.java
+++ ViecCanLam/app/src/main/java/com/edu/nguyengiakhanh/vieccanlam/MainActivity.java
@@ -1,24 +1,51 @@
 package com.edu.nguyengiakhanh.vieccanlam;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    List<TASKS> listVCL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = database.getReference("TASKS");
+        //lang nghe va xu ly
+        listVCL = new ArrayList<TASKS>();
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot obj: snapshot.getChildren()){
+                    TASKS task =obj.getValue(TASKS.class);
+                    listVCL.add(task);
+                    Log.w("VCL app","Ten viec can lam : "+ task.getName() );
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
         });
+
     }
 }
